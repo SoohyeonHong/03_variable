033ab6a6231565d112298b455fdded14a486dfa5
SoohyeonHong <55083734+SoohyeonHong@users.noreply.github.com>
2019-09-19 16:11:36 +0900

    03

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+	//½Ç½À 4
+	 
 	int input_int;
 	float input_float;
 	
