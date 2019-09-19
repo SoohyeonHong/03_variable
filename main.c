f662ba798e71e24e556bc4844e7631a3399185c5
SoohyeonHong <55083734+SoohyeonHong@users.noreply.github.com>
2019-09-19 16:07:06 +0900

    03

--- main.c
+++ main.c
@@ -5,37 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!\n\n");
+	int input_int;
+	float input_float;
 	
-	int x;
-	printf("byte of int : %d\n", sizeof(x) );
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
 	
-	short y;
-	printf("byte of short : %d\n", sizeof(y) );
-
-	long z;
-	printf("byte of long : %d\n", sizeof(z) );
+	printf("enter a float : ");
+	scanf("%f", &input_float);
 	
-	char a;
-	printf("byte of char : %d\n", sizeof(a) );
+	printf("integer : %d, float : %f\n", input_int, input_float );
 	
-	unsigned b;
-	printf("byte of unsigned : %d\n", sizeof(b) );
-	
-	float c;
-	printf("byte of float : %d\n", sizeof(c) );
-	
-	double d;
-	printf("byte of double : %d\n", sizeof(d) );
-	
-	long double e;
-	printf("byte of long double : %d\n", sizeof(e) );
-	
-	unsigned int f;
-	printf("byte of unsigned int : %d\n", sizeof(f) );
-	
-	unsigned long g;
-	printf("byte of unsigned long : %d\n", sizeof(g) );
 	
 	return 0;
 }
